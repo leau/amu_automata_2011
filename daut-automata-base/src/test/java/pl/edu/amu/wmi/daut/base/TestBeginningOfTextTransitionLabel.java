c33f228e7cb51bd512def8e82665a4b1741cc8bb
leau <goulix@o2.pl>
2012-01-24 19:19:42 +0100

    Update daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestBeginningOfTextTransitionLabel.java

---
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestBeginningOfTextTransitionLabel.java
@@ -0,0 +1,50 @@
+package pl.edu.amu.wmi.daut.base;
+
+import junit.framework.TestCase;
+
+/**
+* Klasa testujaca klase BeginningOfTextTransitionLabel.
+* 
+*/
+public class TestBeginningOfTextTransitionLabel extends TestCase {
+
+    /**
+     * Test metody doCheckContext.
+     */
+    public final void testDoCheckContext() {
+        BeginningOfTextTransitionLabel trans = new BeginningOfTextTransitionLabel();
+
+        //assertTrue(trans.doCheckContext("", 0))
+        //assertTrue(trans.doCheckContext("precious", 0));
+        //assertTrue(trans.doCheckContext("smart", 0));
+
+        //assertFalse(trans.doCheckContext("lazy", 1));
+        //assertFalse(trans.doCheckContext("selfish", 6));
+
+        //try {
+        //    trans.doCheckContext("water", 11);
+        //    fail();
+        //} catch (PositionOutOfStringBordersException e) {
+        //    assertTrue(true);
+        //}
+
+        //try {
+        //    trans.doCheckContext("milk", 27);
+        //    fail();
+        //} catch (PositionOutOfStringBordersException e) {
+        //    assertTrue(true);
+        //}
+
+       
+        /**
+        * Test atrybutów.
+        */
+
+        //assertEquals(trans.toString(), "larum");
+
+        //assertFalse(trans.isEmpty());
+        //assertFalse(trans.canAcceptCharacter('s'));
+
+    }
+
+}
